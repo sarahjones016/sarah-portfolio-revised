0df3b60eb2fce8feb7dc587f636ff8d76bf14ceb
Sarah Jones <sarahrjones8@gmail.com>
2023-05-25 16:59:00 -0400

    added homepage arrow button

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import Main from './Main';
 
 function App() {
   return (
+ 
     <div className="App">
       <Main />
     </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,8 @@
 import './Main.css';
 import React from 'react'
 import lemon from '../Assets/lemon.mp4'
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 function Main() {
   return (
@@ -8,9 +10,17 @@ function Main() {
         <div className='overlay'></div>
         <video src={lemon} autoPlay loop muted />
         <div className='content'>
-            <h1 className='homepageName'>SARAH JONES</h1>
-            <h3 className='homepageTitle'>Brooklyn Based Software Engineer</h3>
+            <div className='textContent'>
+              <h1 className='homepageName'>SARAH JONES</h1>
+              <h3 className='homepageTitle'>Brooklyn Based Software Engineer</h3>
+            </div>
+            <div className='downArrow'>
+              <IconContext.Provider value={{ size: "25px"}}>
+                  <BsFillArrowDownCircleFill />
+              </IconContext.Provider>
+            </div>  
         </div>
+        
     </div>
   )
 }
